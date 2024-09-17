e4e487518342a5cc149bd9ef992dafb4b830271e
Ahmed Yamany <yamany2862016@gmail.com>
2024-09-17 13:42:47 +0300

    Initial Commit

---
+++ Transact/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Transact
+//
+//  Created by Ahmed Yamany on 17/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Transact/TransactApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactApp.swift
+//  Transact
+//
+//  Created by Ahmed Yamany on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TransactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
